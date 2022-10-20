62cc920016aca04e1386027a5c454de5149632cf
Fulerenn <maciej15s12@gmail.com>
2022-10-20 20:39:49 +0200

    Base setup

---
+++ src/api/Attendance.ts
@@ -0,0 +1,3 @@
+import { Template } from "../Template";
+
+export default class Attendance extends Template {}
---
+++ src/api/Authorization.ts
@@ -0,0 +1,3 @@
+import { Template } from "../Template";
+
+export default class Authorization extends Template {}
